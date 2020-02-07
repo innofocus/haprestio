c66cb55207b5d17ce79f08354be837693d97bc36
nicolas.ferragu@gmail.com <Q3Y30DMUZo7ZuXBk8q3V7dTkaxa8NMmD>
2020-02-07 16:43:01 +0100

    dumb-init waiting for consul

--- docker/haprestio/docker-entrypoint.sh
+++ docker/haprestio/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
 
+echo "Waiting for consul"
+until /etc/haprestio/consul-template/helpers/consul_kv.py put waitingforconsul dumb-init 2> /dev/null; do sleep 1; printf ".";  done
+/etc/haprestio/consul-template/helpers/consul_kv.py delete waitingforconsul
+
 consul-template -config /etc/haprestio/consul-template/consul-template.cfg &
 
 exec "$@"
