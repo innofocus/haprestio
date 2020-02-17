6a9bf3a3d73fb3faa7cf1e5cfc757cc360fbafde
nicolas.ferragu@gmail.com <Q3Y30DMUZo7ZuXBk8q3V7dTkaxa8NMmD>
2020-02-17 17:15:49 +0100

    build all at once

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 pip uninstall haprestio -y
-python3 setup.py bdist_wheel
+python3 setup.py sdist bdist_wheel
 pip install dist/*
 [ -f docker/*whl ] && rm docker/*whl
-mv dist/* docker
+cp dist/*whl docker
